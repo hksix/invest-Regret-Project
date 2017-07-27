b19bfd19ab2a449945e85e78f28ae8be211e1c08
hksix <mh.haseeb@gmail.com>
2017-07-27 13:33:45 -0400

    changing API

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // api for alpha Vantage 5X4II9G2P5S3BJ05
+// api for quandl YiNzVQcDRbgWz1L_khwj
+
 var apiKey = "5X4II9G2P5S3BJ05";
-var URL = "https://www.alphavantage.co/query?"
+// var URL = "https://www.alphavantage.co/query?"
+var URL =  "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?"
 var $form = $('[data-stock-order="form"]');
 var $tickerName = $('[data-role="ticker-name"]');
 var $timeInterval = $('[data-role="time-interval"]');
@@ -8,7 +11,7 @@ var $amountInvested = $('[data-role="amount-invested"]');
 var $startDate = $('[data-role="start-date"]');
 var $endDate = $('[data-role="end-date"]');
 var $dataKeyNameArr = [ ['tickerName', $tickerName], 
-                        ['timeInterval' , $timeInterval],
+                        // ['timeInterval' , $timeInterval],
                         ['amountInvested' , $amountInvested],
                         ['startDate', $startDate],
                         ['endDate', $endDate],
@@ -29,8 +32,24 @@ function getData(URL){
     x.then( function (data){
         $searchDataDict = data;
         console.log(data);
-        })   
+        console.log(getCloseStartData($startDate.val()));
+        console.log(getCloseEndData($endDate.val()));
+
+        });   
 }
+$(document).ready(function() {
+    var date = new Date();
+
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+
+    var today = year + "-" + month + "-" + day;       
+    $("#inputDate2").attr("value", today);
+});
 
 $form.on('submit', function (event){
     event.preventDefault();
@@ -43,6 +62,7 @@ $form.on('submit', function (event){
     // getOrdersFromAPI();
     setUrl($userInputDict);
     getData($completeURL);
+    
 });
 
 function setItemToLocal(arr){
@@ -58,9 +78,10 @@ function arrMaker(arr){
     return $userInputArr;
 }
 function setUrl(arr){
-    var completeURL= URL+'function='+$userInputDict['timeInterval']+"&symbol="+$userInputDict['tickerName']+"&apikey="+apiKey;
+    // var completeURL= URL+'function='+$userInputDict['timeInterval']+"&symbol="+$userInputDict['tickerName']+"&apikey="+apiKey;
+    var completeURL= URL+'date.gte='+$userInputDict['startDate']+"&date.lte="+$userInputDict['endDate']+"&ticker="+$userInputDict['tickerName']+"&qopts.columns=date,close&api_key=YiNzVQcDRbgWz1L_khwj";
     $completeURL = completeURL;
-    console.log (completeURL);
+    return completeURL;
 }
 function dataDict(arr){
     for (var i = 0; i< arr.length; i++){
@@ -73,8 +94,21 @@ function getServerData(){
         console.log(JSON.stringify(data));
     });
 }
+function getCloseStartData(startDateVal){
+    var formatedDate = startDateVal
+     return $searchDataDict["Time Series (Daily)"][startDateVal]['4. close'];
+}
+function getCloseEndData(EndDateVal){
+     return $searchDataDict["Time Series (Daily)"][EndDateVal]['4. close'];
+}
 
 // HH - hitting submit will run function getData. This will populate the $searchDataDict var
 // to search through dict -  $searchDataDict["Monthly Time Series"]['2000-02-29']
 
 // Object {1. open: "98.5000", 2. high: "110.0000", 3. low: "88.1200", 4. close: "89.3700", 5. volume: "1334487600"}
+//$searchDataDict["Monthly Time Series"][($startDate.val())]['1. open'];
+//$searchDataDict["Time Series (Daily)"][($startDate.val())]['1. open'];
+
+// https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=19900101&date.lt=20170601&ticker=MSFT&qopts.columns=date,close&api_key=YiNzVQcDRbgWz1L_khwj
+// https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=19900102&date.lt=19900103&ticker=MSFT&qopts.columns=date,close&api_key=YiNzVQcDRbgWz1L_khwj
+// https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=MSFT&qopts.columns=date,close&api_key=YiNzVQcDRbgWz1L_khwj
