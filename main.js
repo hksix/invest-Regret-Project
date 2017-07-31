1b56d989795d8ac6acddce218e7c5895cfc36bd5
hksix <mh.haseeb@gmail.com>
2017-07-31 15:31:50 -0400

    adding prototype for coins

--- main.js
+++ main.js
@@ -5,10 +5,12 @@
 // var apiKey = "5X4II9G2P5S3BJ05";
 // var URL = "https://www.alphavantage.co/query?"
 var $URL =  "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?"
-var $form = $('[data-stock-order="form1"]');
+var $stockForm = $('[data-stock-order="form1"]');
+var $coinForm = $('[data-stock-order="form2"]');
 var $tickerName = $('[data-role="ticker-name"]');
 var $timeInterval = $('[data-role="time-interval"]');
 var $amountInvested = $('[data-role="amount-invested"]');
+// var $amountInvestedCoin = $('[data-role="amount-invested-coin"]');
 var $startDate = $('[data-role="start-date"]');
 var $endDate = $('[data-role="end-date"]');
 var $dataKeyNameArr = [ ['tickerName', $tickerName], 
@@ -27,7 +29,7 @@ $(document).ready(function() {
     getCurrentDate();
 });
 
-$form.on('submit',function (event){
+$stockForm.on('submit',function (event){
     event.preventDefault();
     dataDict($dataKeyNameArr);
     setUrl();
@@ -136,48 +138,6 @@ function hindsightAmount(){
     return(dataDict($dataKeyNameArr)['amountInvested'] / unAdjustforSplit()) *(getCloseEndData()); 
     
 }
-
-// ------------------------------------------------------------------------
-// for bit coin page
-var cryptoData;
-
-function getCryptoData(){
-    
-    var URLETH = "https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=7605";
-    // var URLETH = "https://www.cryptocompare.com/api/data/coinsnapshot/?fsym=ETH&tsym=USD";
-    var URLBTC = "https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=1182";
-    var x = $.get(URLETH)
-        x.then(function(data){
-            cryptoData = data;
-            getCryptoInfo(data);
-        })
-    
-    
-}
-function getCryptoInfo(data){
-    console.log(data["Data"]);
-    // console.log(data["Data"]["General"]['Name']);
-    // console.log(data["Data"]["General"]['Twitter']);
-    // console.log(data["Data"]["General"]['Description']);
-    // console.log(data["Data"]["General"]['Features']);
-    // console.log(data["Data"]["General"]['StartDate']);
-    // console.log(data["Data"]["ICO"]['StartPrice']);
-}
-
-
-
-
-"https://www.quandl.com/api/v3/datasets/WIKI/MSFT.json?&column_index=7&api_key=YiNzVQcDRbgWz1L_khwj"
-// splits https://www.quandl.com/api/v3/datasets/WIKI/MSFT.json?&column_index=7&api_key=YiNzVQcDRbgWz1L_khwj
-// "https://www.quandl.com/api/v3/datasets/WIKI/"+$userInputDict['tickerName']+".json?column_index=4&"+$userInputDict['startDate']+"&"+$userInputDict['endDate']+'&collapse=monthly&&api_key=YiNzVQcDRbgWz1L_khwj';
-// https://www.quandl.com/api/v3/datasets/WIKI/MSFT.json?&collapse=annual&column_index=4&api_key=YiNzVQcDRbgWz1L_khwj
-
-// https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD gets current price - returns {"USD":195.32}
-// https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=7605%tsyms=USD//for ETH
-// https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=1182 //for BTC
-// https://www.cryptocompare.com/api/data/coinsnapshot/?fsym=ETH&tsym=USD
-// this is the latest JS
-
 function getGraphData(){
     var temp=[];
     var URL = 'https://www.quandl.com/api/v3/datasets/WIKI/'+dataDict($dataKeyNameArr)['tickerName']+'?&collapse=annual&column_index=4&api_key=YiNzVQcDRbgWz1L_khwj';
@@ -224,3 +184,120 @@ var largest = Math.max.apply(null, y);
     };
     new Chartist.Line('.ct-chart', data,options );
 }
+
+// ------------------------------------------------------------------------
+// for bit coin page
+var $coinName = $('[data-role="coin-name"]');
+var $amountInvestedCoin = $('[data-role="amount-invested-coin"]');
+
+$coinForm.on('submit',function (event){
+    event.preventDefault();
+    getCryptoData();   
+});
+
+
+function getCryptoData(){
+    var coinName = $coinName.val();
+    var amountInvested =$amountInvestedCoin.val();
+    var URLinfo;
+    var URLprices;
+    var startPrice;
+    theCoin = new Coin(coinName);
+    if (coinName === "ETH"){
+        URLinfo = "https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=7605";
+        URLprices ="https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&limit=724&aggregate=1";
+        startPrice = 0.71;
+        getURLdata(URLinfo,URLprices);
+    }
+    if (coinName === "BTC"){
+        URLinfo = 'https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=1182';
+        URLprices = 'https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=2500&aggregate=1';
+        startPrice = 68.08;
+        getURLdata(URLinfo,URLprices);
+    }
+    theCoin.setStarting(startPrice);
+    theCoin.setShares(amountInvested);
+    theCoin.setCurrent()
+    theCoin.getInfo(URLinfo)
+        .then(function (){
+
+        });
+}
+
+function getURLdata(URL1,URL2){
+    
+    // var price = $.get(URL2)
+        // price.then(function(data){
+        //     setCryptoPrice(data);
+        // });
+    var currentPrice = $.get("https://min-api.cryptocompare.com/data/price?fsym="+$coinName.val()+"&tsyms=USD")
+        currentPrice.then(function(data){
+                setCryptoCurrentPrice(data);
+                
+        });
+        
+}
+    
+function setCryptoInfo(data){
+    var shortInfo= $(data["Data"]["General"]['Description']);
+    $('#twitter-handle').text("Twitter: " + (data["Data"]["General"]['Twitter']));
+    $('#coin-info').html(shortInfo[0]["innerHTML"]);
+    $('#coin-start-date').text("Inception Date: "+(data["Data"]["General"]['StartDate']));
+}
+
+function setCryptoCurrentPrice(data){
+    // $coinName.val().current(data["USD"]);
+    $('#coin-price').html("Current Price:" +"<br>"+ "$"+ data["USD"]);
+    return data['USD'];
+    
+}
+function getShares(starting){
+    $amountInvestedCoin.val().setShares()
+    var coinShares = ($amountInvestedCoin.val()) / starting;
+    // $coinName.val().shares(coinShares);
+    return Number(coinShares);
+}
+
+
+function Coin(coinName){
+    this.coinName = coinName;
+}
+
+Coin.prototype.setStarting = function(startingPrice){
+    this.starting =startingPrice;   
+}
+Coin.prototype.setCurrent = function(){
+    return $.get("https://min-api.cryptocompare.com/data/price?fsym="+this.coinName+"&tsyms=USD").then(function (data){
+        this.currentPrice = data['USD'];
+        return this.currentPrice;
+    }.bind(this));
+}
+Coin.prototype.setShares = function(amountInvested){
+    this.shares = Number(amountInvested / this.starting);   
+}
+Coin.prototype.getInfo= function(URL){
+    return $.get(URL)
+        .then(function(data){
+            this.twitterHandle = data["Data"]["General"]['Twitter'];
+            this.coinInfo = $(data["Data"]["General"]['Description'])[0]["innerHTML"];
+            this.startDate = data["Data"]["General"]['StartDate'];
+        }.bind(this));
+}
+
+// $('#coin-net-worth-circle-text');
+
+
+
+
+"https://www.quandl.com/api/v3/datasets/WIKI/MSFT.json?&column_index=7&api_key=YiNzVQcDRbgWz1L_khwj"
+// splits https://www.quandl.com/api/v3/datasets/WIKI/MSFT.json?&column_index=7&api_key=YiNzVQcDRbgWz1L_khwj
+// "https://www.quandl.com/api/v3/datasets/WIKI/"+$userInputDict['tickerName']+".json?column_index=4&"+$userInputDict['startDate']+"&"+$userInputDict['endDate']+'&collapse=monthly&&api_key=YiNzVQcDRbgWz1L_khwj';
+// https://www.quandl.com/api/v3/datasets/WIKI/MSFT.json?&collapse=annual&column_index=4&api_key=YiNzVQcDRbgWz1L_khwj
+
+// https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD gets current price - returns {"USD":195.32}
+// https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=7605%tsyms=USD//for ETH
+// https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=1182 //for BTC
+// https://www.cryptocompare.com/api/data/coinsnapshot/?fsym=ETH&tsym=USD
+// for all BTC price info https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=2500&aggregate=1
+// for all ETH price info https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&limit=724&aggregate=1
+
